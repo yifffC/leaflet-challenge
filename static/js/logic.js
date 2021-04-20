26266d26bcf4a473061101f07047e23f75a2187d
yifffC <yifeicao2020@u.northwestern.edu>
2021-04-19 23:51:00 -0500

    add legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,4 @@
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 //  GET color radius call to the query URL
 d3.json(queryUrl).then(function(data) {
@@ -41,7 +41,7 @@ d3.json(queryUrl).then(function(data) {
       weight: 0.5
     };
   }
-  
+
     var quakesLayer = L.geoJson(data, {
       pointToLayer: function(feature, latlng) {
         return L.circleMarker(latlng);
@@ -52,6 +52,7 @@ d3.json(queryUrl).then(function(data) {
         layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
       }
     });
+
     createMap(quakesLayer);
   });
 
@@ -82,4 +83,43 @@ function createMap(quakes){
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+  // details for the legend
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+
+    var limits = ["0-1", "1-2", "2-3", "3-4", "4-5", "5+"];
+    var colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+    var labelsColor = [];
+    var labelsText = [];
+
+    limits.forEach(function(limit, index) {
+      labelsColor.push(`<li style="background-color: ${colors[index]};"></li>`);
+      labelsText.push(`<span class="legend-label">${limits[index]}</span>`)
+    });
+
+    var labelsColorHtml =  "<ul>" + labelsColor.join("") + "</ul>";
+    var labelsTextHtml = `<div id="labels-text">${labelsText.join("<br>")}</div>`;
+  
+    var legendInfo = "<h4>Earthquake<br>Magnitude</h4>" +
+      "<div class=\"labels\">" + labelsColorHtml + labelsTextHtml
+      "</div>";
+    div.innerHTML = legendInfo;
+  
+    return div;
+  }
+
+  // Add legend to the map.
+  legend.addTo(myMap);
 }
